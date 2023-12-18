906c51cdf8f4a05eae1b7f385c54561df6ed3364
Antonio <antonio-emilio_6@outlook.com>
2023-12-18 10:42:55 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projetos/course/entities/User.java
+++ src/main/java/com/projetos/course/entities/User.java
@@ -1,15 +1,26 @@
 package com.projetos.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;//Permite transformar a classe em bytes
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
